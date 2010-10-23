9cac392ff24364da752ae68e326ff85d4ed0764f
Chris Oliver <excid3@gmail.com>
2010-10-23 17:57:34 -0500

    Working redirects and counting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DownloadCounter::Application.routes.draw do
+  resources :downloads
+  match '*path', :to => 'home#index'
+  root :to => "home#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
