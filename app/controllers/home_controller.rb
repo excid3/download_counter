9c9e4ec4506acf432185ee84791be3f840cea6af
Chris Oliver <excid3@gmail.com>
2010-10-23 18:10:07 -0500

    Removed redirect when visiting root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,6 @@ class HomeController < ApplicationController
         @download.update_attribute(:download_count, @download[:download_count] + 1)
         #redirect_to @download[:original_url], :content_type => "application/octet-stream"
       end
-    else
-      redirect_to "http://excid3.com"
     end
   end
 end
