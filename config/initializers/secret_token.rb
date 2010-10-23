09be621cfd3f9975015ae7917506f870767e16da
Chris Oliver <excid3@gmail.com>
2010-10-23 16:45:31 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DownloadCounter::Application.config.secret_token = '2b64ca011ade82620f55c65bda33ffb0b9eae9552188a002d9adfab0c8d11807df672dd5ea44cccec37028c65d88828785a373616819d99423d23b6c5236a7d6'
